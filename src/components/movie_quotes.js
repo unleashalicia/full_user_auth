d728b86e7347e9f782d9b0d90606d972b085dc2a
unleashalicia <alicia.adams.evans@gmail.com>
2018-01-19 15:26:49 -0800

    Routing complete

---
+++ src/components/movie_quotes.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MovieQuotes = props => {
+    return (
+        <div>
+            <h1 className="center-align">Movie Quote</h1>
+            <h3>I'll be back.</h3>
+        </div>
+    )
+}
+
+export default MovieQuotes;
---
+++ src/components/sign_up.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class SignUp extends Component{
+    render(){
+        return (
+            <div>
+                <h1 className="center-align">Sign Up</h1>
+            </div>
+        )
+    }
+}
+
+export default SignUp;
+

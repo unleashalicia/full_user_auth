23ba0984dec35b82c5fa30b8901c29214bb14cb1
unleashalicia <alicia.adams.evans@gmail.com>
2018-01-19 12:39:09 -0800

    Redux setup complete

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware} from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers';
+import {BrowserRouter as Router} from 'react-router-dom';
 
 import App from './components/app';
 
+//State applied as second param as empty so we can add in middleware as third.
+
 ReactDOM.render(
-    <App />,
+    <Provider store={createStore(rootReducer, {}, applyMiddleware(thunk))}>
+        <Router>
+            <App />
+        </Router>
+    </Provider>,
     document.getElementById('root')
 );

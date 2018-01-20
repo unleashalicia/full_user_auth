e6cbf27822c44c04fd8506ed4dddac45e08256e2
unleashalicia <alicia.adams.evans@gmail.com>
2018-01-19 17:26:49 -0800

    Token saved in local storage

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,2 +1,37 @@
 import types from './types';
+import axios from 'axios';
+
+const BASE_URL = "http://api.reactprototypes.com";
+
+
+export function signUp(cred){
+    return dispatch => {
+
+        axios.post(`${BASE_URL}/signup`, cred).then(resp => {
+            console.log("Resp from Signup: ", resp);
+
+            localStorage.setItem('token', resp.data.token);
+
+            dispatch({type: types.SIGN_UP});
+        })
+    }
+}
+
+export function signIn(cred){
+    return dispatch => {
+
+        axios.post(`${BASE_URL}/signin`, cred).then(resp => {
+            console.log("Resp from Sign in: ", resp);
+
+            localStorage.setItem('token', resp.data.token);
+
+            dispatch({type: types.SIGN_IN});
+        }).catch(err => {
+            dispatch({
+                type: types.ERROR,
+                payload: 'Invalid Username or Password.'
+            });
+        });
+    }
+}
 
--- src/reducers/user_reducer.js
+++ src/reducers/user_reducer.js
@@ -1,11 +1,18 @@
 import types from '../actions/types';
 
 const DEFAULT_STATE = {
-    auth: false
+    auth: false,
+    error: ''
 };
 
 export default (state = DEFAULT_STATE, action) => {
     switch(action.type){
+
+        case types.SIGN_IN:
+        case types.SIGN_UP:
+            return {auth: true, error: ''};
+        case types.ERROR:
+            return {auth: false, error: action.payload};
         default:
             return state;
     }
